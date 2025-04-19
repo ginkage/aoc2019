c5427518bf72ebeb8e292d156f04d2b465ed0d00
Ivan Podogov <ginkage@yandex.ru>
2025-04-19 21:44:44 +0100

    Nine

---
+++ 09/09.cpp
@@ -0,0 +1,109 @@
+#include "../lib.hpp"
+
+struct state_t {
+    unordered_map<long, long> program;
+    queue<long> input;
+    queue<long> output;
+    long ip { 0 };
+    long base { 0 };
+    bool halt { false };
+
+    long get(long arg, int mode) {
+        if (mode == 1)
+            return arg;
+        return program[(mode == 0) ? arg : (base + arg)];
+    }
+
+    void put(long arg, int mode, long value) {
+        program[(mode == 0) ? arg : (base + arg)] = value;
+    }
+};
+
+void run(state_t &st) {
+    unordered_map<long, long> &program = st.program;
+    int i = st.ip;
+    while (!st.halt) {
+        long op = program[i++];
+        int m1 = (op / 100) % 10;
+        int m2 = (op / 1000) % 10;
+        int m3 = (op / 10000) % 10;
+        op %= 100;
+
+        if (op == 1) {
+            long arg1 = program[i++];
+            long arg2 = program[i++];
+            long arg3 = program[i++];
+            st.put(arg3, m3, st.get(arg1, m1) + st.get(arg2, m2));
+        }
+        else if (op == 2) {
+            long arg1 = program[i++];
+            long arg2 = program[i++];
+            long arg3 = program[i++];
+            st.put(arg3, m3, st.get(arg1, m1) * st.get(arg2, m2));
+        }
+        else if (op == 3) {
+            long arg1 = program[i++];
+            if (st.input.empty()) break;
+            st.put(arg1, m1, st.input.front());
+            st.input.pop();
+        }
+        else if (op == 4) {
+            long arg1 = program[i++];
+            st.output.push(st.get(arg1, m1));
+        }
+        else if (op == 5) {
+            long arg1 = program[i++];
+            long arg2 = program[i++];
+            if (st.get(arg1, m1))
+                i = st.get(arg2, m2);
+        }
+        else if (op == 6) {
+            long arg1 = program[i++];
+            long arg2 = program[i++];
+            if (!st.get(arg1, m1))
+                i = st.get(arg2, m2);
+        }
+        else if (op == 7) {
+            long arg1 = program[i++];
+            long arg2 = program[i++];
+            long arg3 = program[i++];
+            st.put(arg3, m3, st.get(arg1, m1) < st.get(arg2, m2));
+        }
+        else if (op == 8) {
+            long arg1 = program[i++];
+            long arg2 = program[i++];
+            long arg3 = program[i++];
+            st.put(arg3, m3, st.get(arg1, m1) == st.get(arg2, m2));
+        }
+        else if (op == 9) {
+            long arg1 = program[i++];
+            st.base += st.get(arg1, m1);
+        }
+        else {
+            st.halt = true;
+            break;
+        }
+        st.ip = i;
+    }
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+    state_t state;
+    auto program = split_l(s, ",");
+
+    for (int i = 0; i < program.size(); i++)
+        state.program[i] = program[i];
+
+    state.input.push(2);
+    run(state);
+
+    while (!state.output.empty()) {
+        cout << state.output.front() << endl;
+        state.output.pop();
+    }
+
+    return 0;
+}
+
